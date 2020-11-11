c818805b8e6f58be712c3cdea3d5f52bbaec20d6
Jannik Keye <jannik.keye@gmail.com>
2020-11-11 12:03:58 +0100

    fix: prod url resolving

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,33 @@ import config from "../data/config.json";
 import InfoContainer from "./components/InfoContainer";
 
 const App = () => {
+  const mappedUis = config.uis.map((ui) => {
+    if (process.env.NODE_ENV === "production") {
+      return {
+        ...ui,
+        js: ui.js
+          .replace("http:", window.location.protocol)
+          .replace("localhost", window.location.hostname)
+          .replace(":3000", ""),
+        css: ui.css
+          ? ui.css
+              .replace("http:", window.location.protocol)
+              .replace("localhost", window.location.hostname)
+              .replace(":3000", "")
+          : ui.css,
+      };
+    }
+
+    return ui;
+  });
+
   return (
     <Flex h="100vh" alignItems="center">
       <Box w="75vw" margin="0 auto 0 auto">
         <Heading as="h1">Composer</Heading>
         <Divider marginBottom={20} />
         <SimpleGrid columns={2} spacing={5}>
-          {config.uis.map((ui) => (
+          {mappedUis.map((ui) => (
             <InfoContainer key={ui.id} {...ui}>
               <EmbeddableUi uiDefinition={ui} />
             </InfoContainer>
